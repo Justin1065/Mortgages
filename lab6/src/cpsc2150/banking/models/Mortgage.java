210e018df3ad6d638b4f60b5835e2832f74a5c5d
Justin1065 <jyk@clemson.edu>
2023-10-31 13:57:05 -0400

    Update Mortgage.java

--- lab6/src/cpsc2150/banking/models/Mortgage.java
+++ lab6/src/cpsc2150/banking/models/Mortgage.java
@@ -1,9 +1,12 @@
 package cpsc2150.banking.models;
 
 /**
- * @invariant theHomeCost >= 0 AND theDownPayment >= 0 AND MIN_YEARS <= theYears <= MAX_YEARS
- * @correspondence self.theHomeCost = theHomeCost AND self.theDownPayment = theDownPayment ABD
- *                 self.theYears = theYears AND self.customer = theCustomer
+ * @invariant theHomeCost >= 0 AND theDownPayment >= 0 AND MIN_YEARS <= theYears <= MAX_YEARS AND apr = BASERATE AND
+ *            principle > 0 AND monthlyPayment > 0 AND 0 <= PercentDown < 1 AND debtIncomeRatio > 0
+ * @correspondence self.theHomeCost = theHomeCost AND self.theDownPayment = theDownPayment AND
+ *                 self.theYears = theYears AND self.apr = apr AND
+ *                 self.principle = principle AND self.monthlyPayments = monthlyPayments AND
+ *                 self.percentDown = percentDown AND self.debtIncomeRatio = debtIncomeRatio AND self.customer = theCustomer
  *
  */
 public class Mortgage extends AbsMortgage implements IMortgage {
@@ -37,10 +40,13 @@ public class Mortgage extends AbsMortgage implements IMortgage {
         theYears = years;
         customer = theCustomer;
 
+        // Calculates the percent down
         percentDown = theDownPayment / theHomeCost;
 
+        // Sets the apr to the base apr
         apr = BASERATE;
 
+        // If condition that uses the years passed in to determine how much to add to the apr
         if(theYears < MAX_YEARS) {
             apr = apr + GOODRATEADD;
         }
@@ -48,10 +54,12 @@ public class Mortgage extends AbsMortgage implements IMortgage {
             apr = apr + NORMALRATEADD;
         }
 
+        // If condition that uses the percent down to determine how much to add to the apr
         if(percentDown() < PREFERRED_PERCENT_DOWN) {
             apr = apr + GOODRATEADD;
         }
 
+        // If conditions that determine how much to add to the apr based on the customer credit score
         if(customer.getCreditScore() < BADCREDIT)
         {
             apr = apr + VERYBADRATEADD;
@@ -71,20 +79,26 @@ public class Mortgage extends AbsMortgage implements IMortgage {
 
         apr = apr/MONTHS_IN_YEAR;
 
+        // Calculates the principle
         principle = theHomeCost - theDownPayment;
 
+        // Calculates the number of payments made by the customer using the months in the years and the years passed in
         double numPayments = MONTHS_IN_YEAR * years;
+
+        // Calculates the monthly payment using the apr, principle, and number of payments
         monthlyPayment = (apr * principle) / (1 - Math.pow((1 + apr), (-numPayments)));
 
         debtIncomeRatio = (monthlyPayment * MONTHS_IN_YEAR)/ customer.getIncome();
     }
 
+    // Returns the percentDown
     private double percentDown() {
 
         return percentDown;
     }
     public boolean loanApproved() {
 
+        // If conditional that determines whether the customer can get a loan or not
         if(((apr * MONTHS_IN_YEAR) < RATETOOHIGH) && (percentDown() >= MIN_PERCENT_DOWN) && (debtIncomeRatio <= DTOITOOHIGH)) {
             return true;
         }
@@ -93,22 +107,26 @@ public class Mortgage extends AbsMortgage implements IMortgage {
         }
     }
 
+    // Returns the monthly payment
     public double getPayment() {
 
         return monthlyPayment;
 
     }
 
+    // Returns the apr
     public double getRate() {
 
         return apr * MONTHS_IN_YEAR;
     }
 
+    // Returns the principle
     public double getPrincipal() {
 
         return principle;
     }
 
+    // Returns the years
     public int getYears() {
 
         return theYears;
