b094ae7f87fc4237a0599019934493069ae41c66
Justin1065 <jyk@clemson.edu>
2023-10-31 00:35:38 -0400

    Contracts and stuff

--- lab6/src/cpsc2150/banking/models/Mortgage.java
+++ lab6/src/cpsc2150/banking/models/Mortgage.java
@@ -1,11 +1,29 @@
 package cpsc2150.banking.models;
 
+/**
+ * @invariant theHomeCost >= 0 AND theDownPayment >= 0 AND MIN_YEARS <= theYears <= MAX_YEARS
+ * @correspondence self.theHomeCost = theHomeCost AND self.theDownPayment = theDownPayment ABD
+ *                 self.theYears = theYears AND self.customer = theCustomer
+ *
+ */
 public class Mortgage extends AbsMortgage implements IMortgage {
 
     private double theHomeCost;
     private double theDownPayment;
     private int theYears;
     ICustomer customer;
+
+    /**
+     * This creates a new object to keep track information for a Mortgage instance.
+     *
+     * @param homeCost the cost of the customer's home
+     * @param downPayment the customer's down payment for the house
+     * @param years the amount of years for the loan
+     * @param theCustomer an instance of the Customer object to get info about the customer
+     *
+     * @pre homeCost >= 0 AND downPayment >= 0 AND MIN_YEARS <= years <= MAX_YEARS
+     * @post theHomeCost = homeCost AND theDownPayment = downPayment AND theYears = years AND customer = theCustomer
+     */
     public Mortgage(double homeCost, double downPayment, int years, ICustomer theCustomer) {
 
         theHomeCost = homeCost;
